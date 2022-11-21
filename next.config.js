8224e5bc71277276dc480cdbf7046ce8f54bfa70
meyer <github.com@meyer.fm>
2022-11-21 18:02:26 -0500

    Run prettier

--- next.config.js
+++ next.config.js
@@ -2,36 +2,34 @@ const withCSS = require("@zeit/next-css");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const withFonts = require("next-fonts");
 
-module.exports =
-  withCSS(
-    withFonts({
-      pwa: {
-        dest: 'public'
-      },
-      webpack: (config, { isServer }) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-          config.node = {
-            fs: "empty",
-          };
-        }
-        config.optimization.minimizer = []
-        config.optimization.minimize = false;
+module.exports = withCSS(
+  withFonts({
+    pwa: {
+      dest: "public",
+    },
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: "empty",
+        };
+      }
+      config.optimization.minimizer = [];
+      config.optimization.minimize = false;
 
-        config.module.rules.push(
-          {
-            test: /\.sqrl$/,
-            use: "raw-loader",
-          });
+      config.module.rules.push({
+        test: /\.sqrl$/,
+        use: "raw-loader",
+      });
 
-        config.plugins.push(
-          new MonacoWebpackPlugin({
-            languages: ["cpp"],
-            filename: "static/[name].worker.js",
-          })
-        );
-        return config;
-      },
-      cssLoaderOptions: { url: false },
-    })
-  );
+      config.plugins.push(
+        new MonacoWebpackPlugin({
+          languages: ["cpp"],
+          filename: "static/[name].worker.js",
+        })
+      );
+      return config;
+    },
+    cssLoaderOptions: { url: false },
+  })
+);
