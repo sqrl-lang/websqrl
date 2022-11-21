237f809a775c1d4d291283af989e12414c9d8e0e
Josh Yudaken <j@yud.co.za>
2022-11-21 18:36:24 -0500

    Split pages into wikipedia and twitter

---
+++ pages/wikipedia.tsx
@@ -0,0 +1,10 @@
+import { StreamPage } from './_page';
+import sampleCode from "../src/wikipedia-sample.sqrl";
+
+export default function TwitterPage() {
+    return <StreamPage
+        urlPrefix='https://sqrl-aws-diffs.s3.amazonaws.com/v1/en.wikipedia.org/'
+        extractDate={(payload) => new Date(payload.meta.dt)}
+        sampleCode={sampleCode}
+    ></StreamPage>;
+}
